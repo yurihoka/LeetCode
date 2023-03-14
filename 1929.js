639f7761339d724ed7000fb097e88aa63dbdad2a
Yurika Hokama <yurikahokama@yu01.local>
2023-03-14 16:43:25 +0900

    solved 1929

---
+++ 1929.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  const ans = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[i]);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[i]);
+  }
+  
+  return ans;
+};
