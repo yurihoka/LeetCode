a4c411a7f3d35c844f8641a278e751832de3b006
Yurika Hokama <yurikahokama@yu01.local>
2023-03-14 22:45:02 +0900

    revised array

--- 283.js
+++ 283.js
@@ -3,17 +3,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  let j = 0;
-  let i = 1;
-  while (i < nums.length) {
-    if (nums[j] === 0 && nums[i] !== 0) {
-      nums[j] = nums[i];
-      nums[i] = 0;
-      j++;
+  let i = 0;
+  let j = 1;
+
+  while (j < nums.length) {
+    if (nums[i] === 0 && nums[j] !== 0) {
+      nums[i] = nums[j];
+      nums[j] = 0;
+      i++;
     }
-    if (nums[j] !== 0) {
-      j++;
+    if (nums[i] !== 0) {
+      i++;
     }
-    i++;
+    j++;
   }
 };
