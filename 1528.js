29ab4e00d6c18d152dded4aeebe944c5c086a173
Yurika Hokama <yurikahokama@yu01.local>
2023-03-17 22:02:17 +0900

    solved 1528

---
+++ 1528.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+  const sorted = indices.slice();
+  let result = [];
+
+  for (let i = 0; i < indices.length; i++) {
+    result[sorted[i]] = s[i];
+  }
+  
+  return result.join("");
+};
