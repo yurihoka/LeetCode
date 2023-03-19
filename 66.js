601f31ee9db4c697d91c08bcd432db301d5c687f
Yurika Hokama <yurikahokama@yu01.local>
2023-03-19 20:44:12 +0900

    revised Line7, 9, 14

--- 66.js
+++ 66.js
@@ -2,18 +2,18 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
-    for(let i = digits.length-1; i >= 0; i--) {
-        if(digits[i] != 9) {
-            digits[i]++;
-            return digits;
-        }
-         if(i === 0) {
-                digits[i] = 0;
-                digits.unshift(1);
-                return digits;
-            }
-        digits[i] = 0;
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] !== 9) {
+      digits[i]++;
+      break;
     }
-    return digits;
+    if (i === 0) {
+      digits[i] = 0;
+      digits.unshift(1);
+      break;
+    }
+    digits[i] = 0;
+  }
+  return digits;
 };
