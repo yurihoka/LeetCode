6bf1f5e3ef39de2410af2093657c6ab4f359bb81
Yurika Hokama <yurikahokama@yu01.local>
2023-03-19 22:33:43 +0900

    revised 1672

--- 1672.js
+++ 1672.js
@@ -4,9 +4,10 @@
  */
 var maximumWealth = function (accounts) {
   let result = 0;
-  let temp = 0;
 
   for (let i = 0; i < accounts.length; i++) {
+    let temp = 0;
+
     for (let j = 0; j < accounts[i].length; j++) {
       temp += accounts[i][j];
     }
