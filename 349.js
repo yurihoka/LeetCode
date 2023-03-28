c6dd9b9f1cf64fdaae5107879dcc0233103b4226
Yurika Hokama <yurikahokama@yu01.local>
2023-03-28 11:52:12 +0900

    L7: revised let to const

--- 349.js
+++ 349.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-  let result = [];
+  const result = [];
 
   for (let i = 0; i < nums1.length; i++) {
     for (let j = 0; j < nums2.length; j++) {
