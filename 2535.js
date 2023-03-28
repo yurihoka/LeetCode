0308eecd4ab8b0b501fa4f56bff681e181165889
Yurika Hokama <yurikahokama@yu01.local>
2023-03-28 12:06:55 +0900

    L6: added the declaretion code(sorted), L9: added the declaretion code(diff), L18: added Math.abs()

--- 2535.js
+++ 2535.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var differenceOfSum = function (nums) {
+  const sorted = [...nums].sort((a, b) => a - b);
   let eleSum = 0;
   let digitSum = 0;
+  let diff = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    eleSum += nums[i];
-
-    while (nums[i] > 0) {
-      digitSum += nums[i] % 10;
-      nums[i] = Math.floor(nums[i] / 10);
+  for (let i = 0; i < sorted.length; i++) {
+    eleSum += sorted[i];
+    while (sorted[i] > 0) {
+      digitSum += sorted[i] % 10;
+      sorted[i] = Math.floor(sorted[i] / 10);
     }
   }
-  
-  let diff = eleSum - digitSum;
+  diff = Math.abs(eleSum - digitSum);
   return diff;
 };
