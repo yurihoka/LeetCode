0c3dc32e1a773e8c37e7429b7d44b37268f2df3f
Yurika Hokama <yurikahokama@yu01.local>
2023-04-25 23:52:08 +0900

    solved 1588, 1816, 2367

---
+++ 1588.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumOddLengthSubarrays = function (arr) {
+  let output = 0;
+
+  for (let i = 1; i <= arr.length; i += 2) {
+    for (let j = 0; j <= arr.length - i; j++) {
+      for (let k = 0; k < i; k++) {
+        output += arr[j + k];
+      }
+    }
+  }
+
+  return output;
+};
