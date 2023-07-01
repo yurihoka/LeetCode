747a37f0d277df8c5395da3ef231c3dfac43197c
Yurika Hokama <yurikahokama@yu01.local>
2023-06-30 19:09:33 -0700

    modified the variable name

--- 1351.js
+++ 1351.js
@@ -2,15 +2,15 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var countNegatives = function (grid) {
-  let negativeSum = 0;
+var countNegatives = function(grid) {
+  let count = 0;
 
   for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] < 0) {
-        negativeSum++;
+      for (let j = 0; j < grid[i].length; j++) {
+         if (grid[i][j] < 0) {
+          count++;
+         }
       }
-    }
   }
-  return negativeSum;
+  return count;
 };
