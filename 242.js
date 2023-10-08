dee7064f63beab227459d28820701655b6fbe9a0
Yurika Hokama <yurihoka@gmail.com>
2023-10-07 20:56:26 -0700

    revise: 242.js

--- 242.js
+++ 242.js
@@ -7,14 +7,5 @@ var isAnagram = function (s, t) {
   const sortedS = s.split("").sort();
   const sortedT = t.split("").sort();
 
-  if (s.length !== t.length) {
-    return false;
-  }
-
-  for (let i = 0; i < s.length; i++) {
-    if (sortedS[i] !== sortedT[i]) {
-      return false;
-    }
-  }
-  return true;
+  return sortedS.join("") === sortedT.join("");
 };
