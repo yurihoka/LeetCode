abe1ff69ac59508afad75b917040e5754272ca71
Yurika Hokama <yurihoka@gmail.com>
2023-10-10 16:17:28 -0700

    revise 2733: use conditional operator

--- 2733.js
+++ 2733.js
@@ -3,11 +3,7 @@
  * @return {number}
  */
 var findNonMinOrMax = function (nums) {
-  const sortedNums = nums.sort((a, b) => a - b);
+  const sortedNums = [...nums].sort((a, b) => a - b);
 
-  if (sortedNums.length > 2) {
-    return sortedNums[1];
-  } else {
-    return -1;
-  }
+  return sortedNums.length > 2 ? sortedNums[1] : -1;
 };
