754172a2662c801b5892477254273629b806903c
Yurika Hokama <yurihoka@gmail.com>
2023-10-27 11:49:37 -0700

    solved 2119, 2169, 2710

---
+++ 2169.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var countOperations = function (num1, num2) {
+  let count = 0;
+
+  while (num1 && num2 !== 0) {
+    num1 > num2 ? (num1 -= num2) : (num2 -= num1);
+    count++;
+  }
+  return count;
+};
