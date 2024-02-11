6912b3aa21b29782df02c2433e954c00bcc50585
Yurika Hokama <yurihoka@gmail.com>
2024-02-11 15:26:54 -0800

    resolve 1122

--- 1122.js
+++ 1122.js
@@ -4,21 +4,22 @@
  * @return {number[]}
  */
 var relativeSortArray = function (arr1, arr2) {
-  const ans = [];
+  const sortArr1 = [];
 
   for (let i = 0; i < arr2.length; i++) {
     for (let j = 0; j < arr1.length; j++) {
       if (arr2[i] === arr1[j]) {
-        ans.push(arr1[j]);
+        sortArr1.push(arr1[j]);
         arr1.splice(j, 1);
         j--;
       }
     }
   }
   arr1.sort((a, b) => a - b);
-  return [...ans, ...arr1];
+  return [...sortArr1, ...arr1];
 };
 
 // console.log(
 //   relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6])
 // );
+// console.log(relativeSortArray([28, 6, 22, 8, 44, 17], [22, 28, 8, 6]));
