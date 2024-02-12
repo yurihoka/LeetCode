f3de81cd394068b3f9d9b2cb187875f0e4feb4a3
Yurika Hokama <yurihoka@gmail.com>
2024-02-11 21:29:44 -0800

    solve 1550

---
+++ 1550.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function (arr) {
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      count++;
+      if (count === 3) {
+        return true;
+      }
+    } else {
+      count = 0;
+    }
+  }
+  return false;
+};
+
+// console.log(threeConsecutiveOdds([2, 6, 4, 1]));
+// console.log(threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]));
+// console.log(threeConsecutiveOdds([1, 2, 1, 1]));
