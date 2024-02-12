0fd60b581508b80b651c99e828938f9973f19169
Yurika Hokama <yurihoka@gmail.com>
2024-02-11 16:02:15 -0800

    resolve 1394

--- 1394.js
+++ 1394.js
@@ -3,15 +3,18 @@
  * @return {number}
  */
 var findLucky = function (arr) {
-  const result = [];
-  
+  const luckyInt = [];
+  let temp = 0;
+
   for (let i = 0; i < arr.length; i++) {
-    if (result.indexOf(arr[i]) === -1) {
-      const freq = arr.filter((el) => el === arr[i]).length;
-      if (freq === arr[i]) result.push(arr[i]);
+    if (luckyInt.indexOf(arr[i]) === -1) {
+      temp = arr.filter((el) => el === arr[i]).length;
+      if (temp === arr[i]) luckyInt.push(arr[i]);
     }
   }
-  return result.length ? Math.max(...result) : -1;
+  return luckyInt.length ? Math.max(...luckyInt) : -1;
 };
 
-// findLucky([2, 2, 3, 4]);
+// console.log(findLucky([2, 2, 3, 4]));
+// console.log(findLucky([1, 2, 2, 3, 3, 3]));
+// console.log(findLucky([2, 2, 2, 3, 3]));
