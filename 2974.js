4890c9f14ab8c21920f10d7088d120af100be48f
Yurika Hokama <yurihoka@gmail.com>
2024-09-10 13:56:57 -0700

    Modified if condition

--- 2974.js
+++ 2974.js
@@ -7,13 +7,11 @@ var numberGame = function (nums) {
   let orderedNums = [];
 
   for (let i = 0; i < sortedNums.length; i++) {
-    if (i === 0 || i % 2 === 0) {
+    if (i % 2 === 0) {
       orderedNums.push(sortedNums[i + 1]);
-    } else if (i % 2 === 1) {
+    } else {
       orderedNums.push(sortedNums[i - 1]);
     }
   }
   return orderedNums;
 };
-
-console.log(numberGame([5, 4, 2, 3]));
